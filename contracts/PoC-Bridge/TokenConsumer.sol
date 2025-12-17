ef8d20aae1a9bf5a388abcdc2014381b9b6b10b9
Olya Tariielashvili <olya@Olyas-MacBook-Pro.local>
2025-12-17 13:40:52 +0100

    Fix bug in TokenConsumer contract

--- contracts/PoC-Bridge/TokenConsumer.sol
+++ contracts/PoC-Bridge/TokenConsumer.sol
@@ -5,15 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenConsumer {
     IERC20 public token;
-
     constructor(address tokenAddress) {
         token = IERC20(tokenAddress);
     }
-
     function deposit(uint256 amount) external {
         token.transferFrom(msg.sender, address(this), amount);
     }
-   
     function getBalance() external view returns (uint256) {
         return token.balanceOf(address(this));
     }
